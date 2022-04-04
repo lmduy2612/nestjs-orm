098ac5c6d8b638c4df27bd7ac8c512f58b879668
duylm <duylm@nhanlucsieuviet.com>
2022-04-04 10:34:32 +0700

    feat: add config orm

--- README.md
+++ README.md
@@ -2,12 +2,6 @@
   <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo_text.svg" width="320" alt="Nest Logo" /></a>
 </p>
 
-## Description
-
-Development by Dinh Xuan Duong
-<br/>
-email: duong120798@gmail.com && xuanduong.kma@gmail.com
-
 ## Installation
 
 ```bash
--- src/orm-config.ts
+++ src/orm-config.ts
@@ -1,12 +1,20 @@
 import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const config: MysqlConnectionOptions = {
   type: 'mysql',
+  // database: process.env.DB_NAME,
+  // username: process.env.DB_USER,
+  // password: process.env.DB_PASS,
+  // port: parseInt(process.env.DB_PORT),
+  // host: process.env.DB_HOST,
   database: 'nest',
   username: 'root',
-  password: 'root',
-  port: 3306,
-  host: '127.0.0.1',
+  password: 'secret',
+  port: 3308,
+  host: '10.0.69.115',
   entities: ['dist/models/**/*.entity{.ts,.js}'],
   synchronize: false,
   dropSchema: false,
